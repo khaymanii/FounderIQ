729683847fd5b19f23e0007ee599b1bdcb6f655a
khaymanii <kennethakpo61@gmail.com>
2025-08-31 13:45:46 +0100

    refactor: remove web app manifest and related links from index.html and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -28,16 +28,6 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "/favicon-16x16.png",
-            sizes: "16x16",
-            type: "image/png",
-          },
-          {
-            src: "/favicon-32x32.png",
-            sizes: "32x32",
-            type: "image/png",
-          },
-          {
             src: "/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
